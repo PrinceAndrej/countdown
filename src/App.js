ced20a7119928adeb3c28c644eff617fa6c9b8e4
PrinceAndrej <steffenelha@hotmail.com>
2023-10-24 11:58:34 +0200

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class CountdownTimer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.calculateCountdown();
+    this.countdownInterval = setInterval(this.calculateCountdown, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.countdownInterval);
+  }
+
+  calculateCountdown = () => {
+    const launchDate = new Date("2023-11-06T21:00:00Z");
+    const now = new Date();
+    const distance = launchDate - now;
+
+    if (distance > 0) {
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      this.setState({
+        days,
+        hours,
+        minutes,
+        seconds,
+      });
+    } else {
+      clearInterval(this.countdownInterval);
+      this.setState({
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      });
+    }
+  };
+
+  render() {
+    const { days, hours, minutes, seconds } = this.state;
+
+    return (
+      <section className="main">
+        <h1>WE`RE LAUNCHING SOON</h1>
+        <div className="container">
+          <div className="countdown-item">
+            <div className="flipdown" id="flipdown-days">
+              {`${days}`}
+            </div>
+            <p>DAYS</p>
+          </div>
+          <div className="countdown-item">
+            <div className="flipdown" id="flipdown-hours">
+              {`${hours}`}
+            </div>
+            <p>HOURS</p>
+          </div>
+          <div className="countdown-item">
+            <div className="flipdown" id="flipdown-minutes">
+              {`${minutes}`}
+            </div>
+            <p>MINUTES</p>
+          </div>
+          <div className="countdown-item">
+            <div className="flipdown" id="flipdown-seconds">
+              {`${seconds}`}
+            </div>
+            <p>SECONDS</p>
+          </div>
+        </div>
+        <div className="svgContainer"></div>
+      </section>
+    );
+  }
 }
 
-export default App;
+export default CountdownTimer;
